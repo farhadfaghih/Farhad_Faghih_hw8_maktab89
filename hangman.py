887bbe76688d7a4917be5b2fac08acc9bd040247
Farhad <fd.faghih@gmail.com>
2022-12-28 16:00:25 +0330

    change method pick_word of class Bank lines 37 & 39

--- hangman.py
+++ hangman.py
@@ -1,11 +1,12 @@
 from random import choice
 import re
 
+
 class Bank:
-    colours = ['red','blue']
-    animals = ['dog','cat']
-    topic_names = ['Colours','Animals']
-    topics = {'Colours':colours,'Animals':animals}
+    colours = ['red', 'blue']
+    animals = ['dog', 'cat']
+    topic_names = ['Colours', 'Animals']
+    topics = {'Colours': colours, 'Animals': animals}
     api = 'https://api.api-ninjas.com/v1/randomword'
     api_key = 'FRkfTIwrgLLk+4TIMd+NMA==m6isKOfXzCLPgdGz'
 
@@ -16,13 +17,13 @@ class Bank:
         self.letters_guessed_counter = 0
         self.not_solved = True
         self.letters_already_guessed = []
-        
+
     def pick_topic(self):
         self.current_topic = choice(self.topic_names)
         print(f'Topic: {self.current_topic}')
 
     def get_word(self):
-        response = requests.get(f"{self.api}", headers={'X-Api-Key': f"{self.api_key}"}, params={type:'noun'})
+        response = requests.get(f"{self.api}", headers={'X-Api-Key': f"{self.api_key}"}, params={type: 'noun'})
         if response.status_code == 200:
             word = json.loads(response.text)
             self.api_response_status = True
@@ -33,13 +34,14 @@ class Bank:
 
     def pick_word(self):
         self.current_word = choice(self.topics[self.current_topic])
-        for i in self.current_word:
+        for i in range(len(self.current_word)):  # added len()
             self.current_word_display.append('_')
-        print(f'Word is {self.current_word} letters long.')
+        print(f'Word is {len(self.current_word)} letters long.')  # added len()
         print(self.current_word_display)
 
     def check_solve(self):
         self.not_solved = self.letters_guessed_counter < len(self.current_word)
+
 
 class Player:
     def __init__(self):
@@ -56,19 +58,19 @@ class Processes:
         pass
 
     def validate_user_input(self, player):
-        expression=re.match('(?i)[a-a]',player.answer)
-        if expression == None or len(player.answer)>1:
+        expression = re.match('(?i)[a-a]', player.answer)
+        if expression == None or len(player.answer) > 1:
             print('\nPlease guess a single alphabet')
         else:
             player.guess_validation_incomplete = False
 
-    def check_answer_update_lives(self,bank,player):
+    def check_answer_update_lives(self, bank, player):
         if player.answer in bank.letters_already_guessed:
             print('\nLetter already guessed.')
-                
+
         elif player.answer not in bank.current_word:
             player.lives -= 1
-            print('\nNope!')            
+            print('\nNope!')
             print('Lives remaining: {}'.format(player.lives))
             bank.letters_already_guessed.append(player.answer)
 
@@ -79,7 +81,8 @@ class Processes:
                     bank.letters_guessed_counter += 1
                     bank.letters_already_guessed.append(player.answer)
                     print('\nNice!')
-    
+
+
 class Main:
     def __init__(self):
         pass
@@ -88,7 +91,7 @@ class Main:
         word_bank = Bank()
         player1 = Player()
         game = Processes()
-        
+
         word_bank.pick_topic()
         word_bank.pick_word()
 
@@ -96,7 +99,7 @@ class Main:
             while player1.guess_validation_incomplete:
                 player1.guess()
                 game.validate_user_input(player1)
-                game.check_answer_update_lives(word_bank,player1)
+                game.check_answer_update_lives(word_bank, player1)
             print(word_bank.current_word_display)
             player1.guess_validation_incomplete = True
             word_bank.check_solve()
@@ -112,7 +115,8 @@ class Main:
         print('\n')
         if replay.upper() == 'X':
             break
-    
+
+
 Play = Main()
 Play
 del Play
